ad2ce7b9317c86e199e6a0091754840210e43b00
Bryaan159 <00057720@uca.edu.sv>
2022-03-20 20:05:43 -0600

    Se agrego una idea para menu de Dashboard

---
+++ dashboard/LandingPageMenu/app/src/main/java/com/bryan159/landingpagemenu/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bryan159.landingpagemenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
